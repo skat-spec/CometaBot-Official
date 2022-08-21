a041609bbd18216929923aec70c0db3dd56d8e59
skat-spec <66431558+skat-spec@users.noreply.github.com>
2022-08-21 22:30:41 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,8 @@
 const { Client, Collection ,Intents } = require("discord.js")
 
-const client = new Client({  intents: [
-  Intents.FLAGS.GUILDS,
-  Intents.FLAGS.GUILD_MEMBERS,
-  Intents.FLAGS.GUILD_BANS,
-  Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS,
-    Intents.FLAGS.GUILD_INTEGRATIONS,
-  	Intents.FLAGS.GUILD_WEBHOOKS,
-  	Intents.FLAGS.GUILD_INVITES,
-	  Intents.FLAGS.GUILD_VOICE_STATES,
-   	Intents.FLAGS.GUILD_PRESENCES,
-	  Intents.FLAGS.GUILD_MESSAGES,
-  Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
-    Intents.FLAGS.GUILD_MESSAGE_TYPING,
-  	Intents.FLAGS.DIRECT_MESSAGES,
-  	Intents.FLAGS.DIRECT_MESSAGE_REACTIONS,
-	  Intents.FLAGS.DIRECT_MESSAGE_TYPING
-]
-})
+const client = new Client({  intents: 32767 })
 
 client.commands = new Collection()
 client.aliases = new Collection()
 
-client.login("OTAzOTg5MDcyMjM2ODAyMTAw.YX0_bg.3ZeuG4V31pHFQtLs7r_hCaW7Aqo")
+client.login(process.env.Token_BotUB)
